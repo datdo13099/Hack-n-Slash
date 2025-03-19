ee9ad51f253561b3765beecf3050869ea4a4c5d0
datdo13099 <coderdatdo@gmail.com>
2025-03-19 11:01:08 +0700

    Fix bug !!!

--- Hack 'n' Slash/main.cpp
+++ Hack 'n' Slash/main.cpp
@@ -10,58 +10,62 @@
 
 int main(int argc, char** agv)
 {
-    // khởi tạo SDL
+    // khoi tao SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
-        cout << "lỗi khởi tạo SDL" << endl;
+        cout << "loi khoi tao SDL" << endl;
         return 1;
     }
-    // tạo cửa sổ
+
+    // tao cua so
     SDL_Window* window = SDL_CreateWindow("Hack 'n' Slash", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         Globals::ScreenWidth * Globals::ScreenScale, Globals::ScreenHeight * Globals::ScreenScale,
         SDL_WINDOW_SHOWN); // SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN
     if (window == nullptr)
     {
         SDL_Quit();
-        cout << "lỗi tạo cửa sổ" << endl;
+        cout << "loi tao cua so" << endl;
         return 1;
     }
 
-    // tạo bộ vẽ
+    // tao bo ve
     Globals::renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (Globals::renderer == nullptr)
     {
         cleanup(window);
         SDL_Quit();
-        cout << "lỗi tạo bộ vẽ" << endl;
+        cout << "loi tao bo ve" << endl;
         return 1;
     }
-    // thiết lập kích thước vẽ trước khi phóng to lên kích thước màn hình
+
+    // thiet lap kich thuoc ve truoc khi phong to len kich thuoc man hinh
     SDL_RenderSetLogicalSize(Globals::renderer, Globals::ScreenWidth, Globals::ScreenHeight);
 
-    // khởi tạo SDL_image
+    // khoi tao SDL_image
     if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG)
     {
         SDL_Quit();
-        cout << "lỗi khởi tạo SDL_image" << endl;
-        return 1;
-    }
-    // khởi tạo SDL_ttf
-    if (TTF_Init() != 0)
-    {
-        SDL_Quit();
-        cout << "lỗi khởi tạo SDL_ttf" << endl;
-        return 1;
-    }
-    // khởi tạo SDL_mixer
-    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
-    {
-        SDL_Quit();
-        cout << "lỗi khởi tạo SDL_mixer" << endl;
+        cout << "loi khoi tao SDL_image" << endl;
         return 1;
     }
 
-    // khởi tạo game và chạy vòng lặp cập nhật
+    // khoi tao SDL_ttf
+    if (TTF_Init() != 0)
+    {
+        SDL_Quit();
+        cout << "loi khoi tao SDL_ttf" << endl;
+        return 1;
+    }
+
+    // khoi tao SDL_mixer
+    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
+    {
+        SDL_Quit();
+        cout << "loi khoi tao SDL_mixer" << endl;
+        return 1;
+    }
+
+    // khoi tao game va chay vong lap cap nhat
     Game game;
     game.update();
 
